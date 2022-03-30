88b7383b65055265095595c16cca0c66e62bd0a5
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-03-30 16:10:12 +0200

    navbar intent

--- app/src/main/java/etu/polytech/foodbm/NavBarFragment.java
+++ app/src/main/java/etu/polytech/foodbm/NavBarFragment.java
@@ -1,5 +1,6 @@
 package etu.polytech.foodbm;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,14 +10,32 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class NavBarFragment extends Fragment {
-    TextView textView;
+    private Button homeButton;
+    private Button voucherButton;
+    private Button planButton;
+    private Button settingButton;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_nav_bar, container, false);
+        homeButton = view.findViewById(R.id.homeButton);
+        voucherButton = view.findViewById(R.id.voucherButton);
+        planButton = view.findViewById(R.id.planButton);
+        settingButton = view.findViewById(R.id.settingButton);
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
 }
