a3495e5d591a317d8df54eb001274e655a531987
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-04-20 17:30:54 +0200

    add listview inside plan

---
+++ app/src/main/java/etu/polytech/foodbm/ListAdaptaterPlan.java
@@ -0,0 +1,39 @@
+package etu.polytech.foodbm;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+
+public class ListAdaptaterPlan extends ArrayAdapter<Plan> {
+    public ListAdaptaterPlan(Context context, ArrayList<Plan> planArrayList) {
+        super(context, R.layout.conso_layout, planArrayList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Plan plan = getItem(position);
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.plan_layout, parent, false);
+        }
+
+        TextView planName = convertView.findViewById(R.id.PlanNameTextView);
+        TextView planValue = convertView.findViewById(R.id.PlanValueTextView);
+        TextView planDate= convertView.findViewById(R.id.PlanDateTextView);
+
+        planName.setText(plan.getName());
+        planValue.setText(String.valueOf(plan.getValue()));
+        planDate.setText(String.valueOf(plan.getDate()));
+
+        return convertView;
+    }
+}
--- app/src/main/java/etu/polytech/foodbm/PlanManagerActivity.java
+++ app/src/main/java/etu/polytech/foodbm/PlanManagerActivity.java
@@ -2,12 +2,11 @@ package etu.polytech.foodbm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class  PlanManagerActivity extends AppCompatActivity {
@@ -17,25 +16,25 @@ public class  PlanManagerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_manager);
-        this.CreatePlan = (Button) findViewById(R.id.addPlanButton);
-        CreatePlan.setOnClickListener(new View.OnClickListener() {
+        //this.CreatePlan = (Button) findViewById(R.id.addPlanButton);
+        /*CreatePlan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(PlanManagerActivity.this, CreatePlanActivity.class);
                 startActivity(intent);
                 finish();
             }
-        });
+        });*/
 
-        ArrayList<Conso> listPlan = new ArrayList<>();
-        listPlan.add(new Conso("Conso perso", 100));
-        listPlan.add(new Conso("Travail Chine", 100));
-        listPlan.add(new Conso("Travail Espagne", 100));
-        for(int i= 0 ; i < 20 ; i++)listPlan.add(new Conso("Travail Espagne n°"+i, 100+i));
+        ArrayList<Plan> listPlan = new ArrayList<>();
+        listPlan.add(new Plan("Conso perso", 100, LocalDate.now()));
+        listPlan.add(new Plan("Travail Chine", 100,LocalDate.now()));
+        listPlan.add(new Plan("Travail Espagne", 100,LocalDate.now()));
+        for(int i= 0 ; i < 20 ; i++)listPlan.add(new Plan("Travail Espagne n°"+i, 100+i,LocalDate.now()));
 
 
-        ListAdaptater listAdaptaterPlan = new ListAdaptater(PlanManagerActivity.this, listPlan);
+        ListAdaptaterPlan listAdaptaterConsoPlan = new ListAdaptaterPlan(PlanManagerActivity.this, listPlan);
         ListView planListView = findViewById(R.id.planListView);
-        planListView.setAdapter(listAdaptaterPlan);
+        planListView.setAdapter(listAdaptaterConsoPlan);
     }
 }
