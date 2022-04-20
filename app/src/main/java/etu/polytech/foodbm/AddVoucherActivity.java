ca54efc494ae7ee7edcd0475f7c3b49ca09ab1ab
AyoubIMAMI <youbima17@gmail.com>
2022-04-20 17:02:44 +0200

    Vouchers activity Images

--- app/src/main/java/etu/polytech/foodbm/AddVoucherActivity.java
+++ app/src/main/java/etu/polytech/foodbm/AddVoucherActivity.java
@@ -1,6 +1,9 @@
 package etu.polytech.foodbm;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
@@ -8,6 +11,7 @@ import android.util.EventLogTags;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -22,6 +26,10 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Calendar;
     public class AddVoucherActivity extends AppCompatActivity {
+
+        private static final int PICK_IMAGE = 100;
+        private ImageView preview;
+        private Button fromGallery;
 
         // creating variables for
         // EditText and buttons.
@@ -147,6 +155,15 @@ import java.util.Calendar;
                     }
                 }
             });
+
+            this.preview = findViewById(R.id.preview);
+            this.fromGallery = (Button) findViewById(R.id.gallery);
+            fromGallery.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    openGallery();
+                }
+            });
         }
 
         private void addDatatoFirebase(String name, String Description, String date) {
@@ -173,6 +190,19 @@ import java.util.Calendar;
                 }
 
             });
+        }
+
+        private void openGallery() {
+            Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+            startActivityForResult(gallery, PICK_IMAGE);
+        }
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data){
+            super.onActivityResult(requestCode, resultCode, data);
+            if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+                Uri imageUri = data.getData();
+                preview.setImageURI(imageUri);
+            }
         }
     }
 
