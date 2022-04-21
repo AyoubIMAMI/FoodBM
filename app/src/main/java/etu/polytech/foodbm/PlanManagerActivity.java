43f4cb176724965262fb5d41878000328ec3e0b1
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-04-21 23:31:52 +0200

    plan detail end and fix plan list

--- app/src/main/java/etu/polytech/foodbm/PlanManagerActivity.java
+++ app/src/main/java/etu/polytech/foodbm/PlanManagerActivity.java
@@ -8,26 +8,27 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class  PlanManagerActivity extends AppCompatActivity {
-    private Button CreatePlan;
+    private TextView CreatePlan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_manager);
-        //this.CreatePlan = (Button) findViewById(R.id.addPlanButton);
-        /*CreatePlan.setOnClickListener(new View.OnClickListener() {
+        this.CreatePlan = (TextView) findViewById(R.id.addPlanButton);
+        CreatePlan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(PlanManagerActivity.this, CreatePlanActivity.class);
                 startActivity(intent);
                 finish();
             }
-        });*/
+        });
 
         ArrayList<Plan> listPlan = new ArrayList<>();
         listPlan.add(new Plan("Conso perso", 100, LocalDate.now()));
