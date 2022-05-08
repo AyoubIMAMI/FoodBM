5da4a13ec725b31cecb98bac9a5743c5466e880c
baidouri <alibaidouri7@gmail.com>
2022-05-09 01:25:41 +0200

    Apparence notification en fonction date expiration

--- app/src/main/java/etu/polytech/foodbm/model/NotificationModel.java
+++ app/src/main/java/etu/polytech/foodbm/model/NotificationModel.java
@@ -3,6 +3,7 @@ package etu.polytech.foodbm.model;
 import static android.content.ContentValues.TAG;
 
 import android.app.Notification;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.collection.ArraySet;
 import androidx.core.app.NotificationCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,6 +21,14 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+
 import etu.polytech.foodbm.VoucherInfo;
 import etu.polytech.foodbm.controller.CurrencyController;
 import etu.polytech.foodbm.controller.NotificationController;
@@ -27,12 +37,22 @@ public class NotificationModel {
     private static int notificationId = 0;
     private NotificationController controller;
     VoucherInfo voucherInfo =new VoucherInfo();
+    List<String> vouch=new ArrayList<>();
+    List<String> image=new ArrayList<>();
     Notification notif;
+    private Bitmap bitmap;
+
 
 
 
     public NotificationModel(NotificationController controller){
         this.controller = controller;
+
+
+
+    }
+
+    public void sendNotification(){
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         DocumentReference datadoc = db.collection("voucher").document(
                 "ROW"
@@ -40,25 +60,43 @@ public class NotificationModel {
         db.collection("voucher")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d(TAG, document.getId() + " => " + document.get("vname"));
-                                voucherInfo.setVName(document.getString("vname"));
+                               Log.d(TAG, document.getId() + " => " + document.get("vdate"));
+                                LocalDate date = LocalDate.now();
+                                 String formattedDate = date.format(DateTimeFormatter
+                                        .ofLocalizedDate(FormatStyle.SHORT));
+
+                               int compar=document.get("vdate").toString().compareTo(formattedDate);
+                               if(compar<0)
+                                vouch.add(String.valueOf(document.get("vname")));
+                                    if(document.get("imageID")!=null){
+                                        image.add(String.valueOf(document.get("imageID")));
+
+                               }
+
+                              }
+
+                            int i;
+                            for(i=0;i<vouch.size();++i){
+                                Log.d(TAG,vouch.toString());
+                                Log.d(TAG,image.toString());
+                                String name = "Coupon "+vouch.get(i)+" a atteint sa date d'expiration";
+                                String description = voucherInfo.getDescription();
+                                notificationId++;
+                                notif = controller.onNotificationSent(name, description, notificationId, NotificationCompat.PRIORITY_DEFAULT);
                             }
+
                         } else {
                             Log.d(TAG, "Error getting documents: ", task.getException());
                         }
                     }
                 });
-    }
 
-    public void sendNotification(){
-        notificationId++;
-        String name = "Coupon "+voucherInfo.getVName()+" a atteint sa date d'expiration";
-        String description = voucherInfo.getDescription();
-        this.notif = this.controller.onNotificationSent(name, description, notificationId, NotificationCompat.PRIORITY_DEFAULT);
+
     }
 
 }
