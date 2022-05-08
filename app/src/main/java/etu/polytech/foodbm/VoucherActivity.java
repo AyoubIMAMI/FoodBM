55524e40afbd7ba786b06f93221172787b300595
baidouri <alibaidouri7@gmail.com>
2022-05-09 01:48:36 +0200

    affichage coupon

--- app/src/main/java/etu/polytech/foodbm/VoucherActivity.java
+++ app/src/main/java/etu/polytech/foodbm/VoucherActivity.java
@@ -4,6 +4,7 @@ import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
@@ -15,6 +16,8 @@ import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -26,7 +29,11 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.ArrayList;
+import java.util.List;
 
 public class VoucherActivity extends AppCompatActivity {
 
@@ -43,7 +50,6 @@ public class VoucherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voucher);
-        mRecyclerView = findViewById(R.id.recyclerView);
 
         NavFragment navFragment = NavFragment.newInstance(R.id.voucherButton);
         getSupportFragmentManager().beginTransaction().replace(R.id.nav_container, navFragment).commit();
@@ -59,10 +65,50 @@ public class VoucherActivity extends AppCompatActivity {
         });
         getdata = findViewById(R.id.AfficherVoucher);
         firestore = FirebaseFirestore.getInstance();
+        TextView itemInfo = (TextView) findViewById(R.id.textView8);
+
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         DocumentReference doc = db.collection("voucher").document();
+        getdata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                DocumentReference datadoc = db.collection("voucher").document(
+                        "ROW"
+                );
+                db.collection("voucher")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
 
-    }
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Log.d(TAG, document.getId() + " => " + document.get("vdate"));
+                                        LocalDate date = LocalDate.now();
+                                        String formattedDate = date.format(DateTimeFormatter
+                                                .ofLocalizedDate(FormatStyle.SHORT));
+
+                                        itemInfo.setText(( document.get("vname").toString()+" "+document.get("vdate")));
+
+
+                                    }
+                                } else {
+                                    Log.d(TAG, "Error getting documents: ", task.getException());
+                                }
+                            }
+                        });
+
+
+            }
+
+
+
+
+            });
+        };
+
+
 
 }
 
